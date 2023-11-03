382740dfec94fe1e61c450b7154b798115007670
cghael <a.p.gorobets@gmail.com>
2023-11-03 15:41:32 +0100

    Change project structure

--- huli_inc/core/utils.py
+++ huli_inc/core/utils.py
@@ -6,11 +6,7 @@ from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 
 class AccountActivationTokenGenerator(PasswordResetTokenGenerator):
     def _make_hash_value(self, user, timestamp):
-        return (
-            str(user.pk) +
-            str(timestamp) +
-            str(user.is_active)
-        )
+        return str(user.pk) + str(timestamp)
 
 
 account_activation_token = AccountActivationTokenGenerator()
